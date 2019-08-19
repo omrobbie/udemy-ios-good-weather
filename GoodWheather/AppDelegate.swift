47e28a6227d774bbb762b11d1eacaa0957461d43
omrobbie <putu.roby@yahoo.com>
2019-08-19 14:37:53 +0800

    Initial Commit

---
+++ GoodWheather/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  GoodWheather
+//
+//  Created by omrobbie on 19/08/19.
+//  Copyright © 2019 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ GoodWheather/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  GoodWheather
+//
+//  Created by omrobbie on 19/08/19.
+//  Copyright © 2019 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
