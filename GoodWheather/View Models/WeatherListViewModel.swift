7efd967066c02ca9dafc7e81e3e8052efa6e512c
omrobbie <putu.roby@yahoo.com>
2019-08-20 13:44:09 +0800

    implement coding keys

--- GoodWheather/View Models/WeatherListViewModel.swift
+++ GoodWheather/View Models/WeatherListViewModel.swift
@@ -14,11 +14,22 @@ struct WeatherListViewModel {
 
 struct WeatherViewModel: Decodable {
     let name: String
-    let main: TemperatureViewModel
+    let currentTemperature: TemperatureViewModel
+
+    private enum CodingKeys: String, CodingKey {
+        case name
+        case currentTemperature = "main"
+    }
 }
 
 struct TemperatureViewModel: Decodable {
     let temperature: Double
     let temperatureMin: Double
     let temperatureMax: Double
+
+    private enum CodingKeys: String, CodingKey {
+        case temperature = "temp"
+        case temperatureMin = "temp_min"
+        case temperatureMax = "temp_max"
+    }
 }
