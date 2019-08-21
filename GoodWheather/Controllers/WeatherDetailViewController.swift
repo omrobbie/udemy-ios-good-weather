f0120bdbda6bbeacfb6e161e121414ad2bcf7250
omrobbie <putu.roby@yahoo.com>
2019-08-21 10:40:18 +0800

    setup new layout for detail weather info

---
+++ GoodWheather/Controllers/WeatherDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherDetailViewController.swift
+//  GoodWheather
+//
+//  Created by omrobbie on 21/08/19.
+//  Copyright © 2019 omrobbie. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WeatherDetailViewController: UIViewController {
+    
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var maxTempLabel: UILabel!
+    @IBOutlet weak var minTempLabel: UILabel!
+}
